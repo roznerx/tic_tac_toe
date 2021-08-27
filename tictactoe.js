3f11d7c1c8d08f633d6dc072aa898a1df7e8034a
roznerx <ignaciorosner@gmail.com>
2021-08-26 21:01:32 -0300

    VS Mode logic completed! Also added a popup start screen in order to make the overall UI better looking and cohesive. Now, onto the VS CPU Mode, and maybe try the minmax algorithm? Also need to add a winning screen popup

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,13 @@
-const playerOneName = document.getElementById("player1name");
-const playerOneScore = document.getElementById("playeronescore");
+//Pop-Up constants
 
-const playerTwoName = document.getElementById("player2name");
-const playerTwoScore = document.getElementById("playertwoscore");
+const modalOverlay = document.getElementById("modal-overlay");
+const modalWindow = document.getElementById("modal-window");
+const modalContent = document.getElementById("modal-content");
+const gameButtons = document.getElementById("game-buttons");
+const popUpVs = document.getElementById("popUpVs");
+const popUpCpu = document.getElementById("popUpCpu");
 
-let playerOneTurn = "";
-let playerTwoTurn = "";
+//Gameboard constants
 
 const box = document.getElementsByClassName("box");
 
@@ -19,25 +21,23 @@ const box6 = document.getElementById("box6");
 const box7 = document.getElementById("box7");
 const box8 = document.getElementById("box8");
 
-const gameBoard = {
-    board: [
-        box0, box1, box2, 
-        box3, box4, box5, 
-        box6, box7, box8
-    ],
-    segments: [
-        //horizontal
-        [box0, box1, box2],
-        [box3, box4, box5],
-        [box6, box7, box8],
-        //vertical
-        [box0, box3, box6],
-        [box1, box4, box7],
-        [box2, box5, box8],
-        //diagonal
-        [box0, box4, box8],
-        [box6, box4, box2]
-    ],
+//Player info constants & variables
+
+const playerOneName = document.getElementById("player1name");
+let playerOneScore = document.getElementById("playeronescore");
+
+const playerTwoName = document.getElementById("player2name");
+let playerTwoScore = document.getElementById("playertwoscore");
+
+let playerOneTurn = "";
+let playerTwoTurn = "";
+
+//GAME
+
+function cleanBoard() {
+    for (let i = 0; i < box.length; i++) {
+        box[i].innerHTML = "";
+    }
 };
 
 function createPlayer(name) {
@@ -49,7 +49,75 @@ function createPlayer(name) {
     }
 };
 
-function movement(p1, p2) {
+function turnChanger() {
+    if (playerOneTurn == "" && playerTwoTurn == "") {
+        playerOneTurn = "ON";
+        playerTwoTurn = "OFF";
+    }
+};
+
+/*
+THE ODIN PROJECT recommends/asks "to store the gameboard as an array inside of a Gameboard object", but I didn't know exactly how to
+make it work within the DOM logic, so it was useless in my code:
+
+const gameBoard = {
+    board: [
+        box0, box1, box2, 
+        box3, box4, box5, 
+        box6, box7, box8
+    ]
+};
+
+Instead, I went for a more straightforward yet repetitive working method. I'm sure it does not follow DRY correctly, but it works.
+*/
+
+function winCon() {
+    if (
+        //horizontal
+        box0.innerHTML == "X" && box1.innerHTML == "X" && box2.innerHTML == "X" ||
+        box3.innerHTML == "X" && box4.innerHTML == "X" && box5.innerHTML == "X" ||
+        box6.innerHTML == "X" && box7.innerHTML == "X" && box8.innerHTML == "X" ||
+        //vertical
+        box0.innerHTML == "X" && box3.innerHTML == "X" && box6.innerHTML == "X" ||
+        box1.innerHTML == "X" && box4.innerHTML == "X" && box7.innerHTML == "X" ||
+        box2.innerHTML == "X" && box5.innerHTML == "X" && box8.innerHTML == "X" ||
+        //diagonal
+        box0.innerHTML == "X" && box4.innerHTML == "X" && box8.innerHTML == "X" ||
+        box6.innerHTML == "X" && box4.innerHTML == "X" && box2.innerHTML == "X") {
+            alert(playerOneName.innerHTML + " wins!");
+            playerOneScore.innerHTML = parseInt(playerOneScore.innerHTML) + 1;
+            cleanBoard();
+            playerOneTurn = "ON";
+            playerTwoTurn = "OFF";
+    } else if (
+        //horizontal
+        box0.innerHTML == "O" && box1.innerHTML == "O" && box2.innerHTML == "O" ||
+        box3.innerHTML == "O" && box4.innerHTML == "O" && box5.innerHTML == "O" ||
+        box6.innerHTML == "O" && box7.innerHTML == "O" && box8.innerHTML == "O" ||
+        //vertical
+        box0.innerHTML == "O" && box3.innerHTML == "O" && box6.innerHTML == "O" ||
+        box1.innerHTML == "O" && box4.innerHTML == "O" && box7.innerHTML == "O" ||
+        box2.innerHTML == "O" && box5.innerHTML == "O" && box8.innerHTML == "O" ||
+        //diagonal
+        box0.innerHTML == "O" && box4.innerHTML == "O" && box8.innerHTML == "O" ||
+        box6.innerHTML == "O" && box4.innerHTML == "O" && box2.innerHTML == "O") {
+            alert(playerTwoName.innerHTML + " wins!");
+            playerTwoScore.innerHTML = parseInt(playerTwoScore.innerHTML) + 1;
+            cleanBoard();
+            playerOneTurn = "ON";
+            playerTwoTurn = "OFF";
+    } else if (
+        box0.innerHTML !== "" && box1.innerHTML !== "" && box2.innerHTML !== "" &&
+        box3.innerHTML !== "" && box4.innerHTML !== "" && box5.innerHTML !== "" &&
+        box6.innerHTML !== "" && box7.innerHTML !== "" && box8.innerHTML !== "") {
+            alert("Draw!");
+            cleanBoard();
+            playerOneTurn = "ON";
+            playerTwoTurn = "OFF";
+    }
+};
+
+function movement() {
     for (let i = 0; i < box.length; i++) {
         box[i].addEventListener("click", () => {
             if (box[i].innerHTML == "") {
@@ -63,53 +131,26 @@ function movement(p1, p2) {
                     playerTwoTurn = "OFF";
                 }
             }
-    
-    
-            if (box0.innerHTML == "X" && box1.innerHTML == "X" && box2.innerHTML == "X") {
-                console.log("WORKINGOK");
-                playerOneScore.innerHTML +=1;
-            }
-    
-    
-    
+        winCon();    
         });
     }
 };
 
-
-
-
-
-function cleanBoard() {
-    for (let i = 0; i < box.length; i++) {
-        box[i].innerHTML = "";
-    }
-};
-
-function turnChanger() {
-    if (playerOneTurn == "" && playerTwoTurn == "") {
-        playerOneTurn = "ON";
-        playerTwoTurn = "OFF";
-    }
-};
-
 function vsPlayerTwoMode(p1, p2) {
+
+    cleanBoard()
     
     p1 = createPlayer();
-    p1.name = prompt("Insert Player 1 name");
-    playerOneName.innerHTML = p1.name;
+    p1.name = playerOneName.innerHTML;
     playerOneScore.innerHTML = "0";
 
     p2 = createPlayer();
-    p2.name = prompt("Insert Player 2 name");
-    playerTwoName.innerHTML = p2.name;
+    p2.name = playerTwoName.innerHTML;
     playerTwoScore.innerHTML = "0";
 
     movement();
-
     turnChanger();
 };
-
 
 const vsPlayerTwo = document.getElementById("vsplayer2");
 vsPlayerTwo.addEventListener("click", () => {
@@ -119,7 +160,18 @@ vsPlayerTwo.addEventListener("click", () => {
 });
 
 function vsCPUMode() {
+
     cleanBoard()
+    
+    p1 = createPlayer();
+    p1.name = playerOneName.innerHTML;
+    playerOneScore.innerHTML = "0";
+
+    p2 = createPlayer();
+    p2.name = "CPU"
+    playerTwoName.innerHTML = p2.name;
+    playerTwoScore.innerHTML = "0";
+
     console.log("NOTYETCODED");
 }
 
@@ -131,3 +183,73 @@ vsCPU.addEventListener("click", () => {
 
 })
 
+//POPUP
+
+popUpVs.addEventListener("click", () => {
+    modalContent.innerHTML = "Insert Player 1 name";
+    modalContent.style.marginLeft = "130px";
+    popUpVs.remove();
+    popUpCpu.remove();
+    
+    let p1NameBar = document.createElement("input");
+    p1NameBar.id = "p1NameBar";
+    gameButtons.appendChild(p1NameBar);
+        
+    let p1NameOk = document.createElement("h2");
+    p1NameOk.class = "popUpButton";
+    p1NameOk.type = "button";
+    p1NameOk.id = "popUpP1NameOk";
+    p1NameOk.innerHTML = "OK";
+    gameButtons.appendChild(p1NameOk);
+
+    p1NameOk.addEventListener("click", () => {
+        playerOneName.innerHTML = p1NameBar.value;
+
+        modalContent.innerHTML = "Insert Player 2 name";
+        modalContent.style.marginLeft = "130px";
+        p1NameBar.remove();
+        p1NameOk.remove();
+
+        let p2NameBar = document.createElement("input");
+        p2NameBar.id = "p2NameBar";
+        gameButtons.appendChild(p2NameBar);
+        
+        let p2NameOk = document.createElement("h2");
+        p2NameOk.class = "popUpButton";
+        p2NameOk.type = "button";
+        p2NameOk.id = "popUpP2NameOk";
+        p2NameOk.innerHTML = "OK";
+        gameButtons.appendChild(p2NameOk);
+
+        p2NameOk.addEventListener("click", () => {
+            playerTwoName.innerHTML= p2NameBar.value;
+            document.body.removeChild(modalOverlay);
+            vsPlayerTwoMode();
+        })
+    });
+});
+
+popUpCpu.addEventListener("click", () => {
+
+    modalContent.innerHTML = "Insert Player name";
+    modalContent.style.marginLeft = "130px";
+    popUpVs.remove();
+    popUpCpu.remove();
+    
+    let p1NameBar = document.createElement("input");
+    p1NameBar.id = "p1NameBar";
+    gameButtons.appendChild(p1NameBar);
+        
+    let p1NameOk = document.createElement("h2");
+    p1NameOk.class = "popUpButton";
+    p1NameOk.type = "button";
+    p1NameOk.id = "popUpP1NameOk";
+    p1NameOk.innerHTML = "OK";
+    gameButtons.appendChild(p1NameOk);
+
+    p1NameOk.addEventListener("click", () => {
+        playerOneName.innerHTML = p1NameBar.value;
+        document.body.removeChild(modalOverlay);
+        vsCPUMode();
+    });
+});
